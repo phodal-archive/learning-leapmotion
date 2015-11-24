8611769d0120f5ff0721b4271babd35ef072313e
Fengda HUANG <h@phodal.com>
2015-11-24 17:53:12 +0800

    try to use api

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ var leapjs      = require('leapjs');
 
 var controller  = new leapjs.Controller();
 
-controller.on('connect', function() {
+controller.on('connect', function(data) {
+	console.log(data);
   console.log("Successfully connected.");
 });
 
-controller.on('deviceConnected', function() {
+controller.on('streamingStarted', function(data) {
+	console.log(data);
   console.log("A Leap device has been connected.");
 });
 
-controller.on('deviceDisconnected', function() {
+controller.on('streamingStopped', function(data) {
+	console.log(data);
   console.log("A Leap device has been disconnected.");
 });
 
